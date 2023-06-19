2afe3597e569515ea8a639479879054f7a0ff949
Topsonray <stopsonray@gmail.com>
2023-06-19 16:20:45 -0700

    if else while

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - write "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
  *                followed by a new line, to standard error.
  *
  * Return: Always 1.
@@ -10,7 +10,7 @@ int main(void)
 {
 	write(2,
 	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	      29);
+	      59);
 
 	return (1);
 }
