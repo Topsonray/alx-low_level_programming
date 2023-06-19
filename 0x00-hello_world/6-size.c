3c1219abd04dac0f68019ade7eaa6194160af772
Topsonray <stopsonray@gmail.com>
2023-06-19 10:48:49 -0700

    we move

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * int main - prints the size of various types on the computer it is compiled and run on
+ * main - prints the size of various types on the computer it is compiled and run on..
  *
  * Return: Always 0.
  */
@@ -14,3 +14,4 @@ int main(void)
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
 	return (0);
+}
