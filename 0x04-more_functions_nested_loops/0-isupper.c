1c3158343f0341d16e5b145b8b8f5d2297eca06a
Topsonray <stopsonray@gmail.com>
2023-06-22 13:23:44 -0700

    first task

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if a letter is uppercase
+ * @x: is the character to be checked
+ * Return: 1 for uppercase or else 0
+ */
+
+int _isupper(int x)
+
+{
+
+	if (x >= 65 && X <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
