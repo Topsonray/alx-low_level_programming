59413c90b55b18c647fe9b3fa25ab2f0eb157f03
Topsonray <stopsonray@gmail.com>
2023-06-22 17:20:57 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - determines if a character is digit
+ * @x: is the parameter to be checked
+ * Return: 1 if true otherwise 0
+ */
+
+int _isdigit(int x)
+
+{
+	if (x >= 48 && x <= 59)
+	{
+	return (1);
+	}
+	return (0);
+}
+
